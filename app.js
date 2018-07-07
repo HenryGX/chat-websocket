56ea3624f73fcd977e262ac9d00de86882733923
Henry Zhang <ws00381493@gmail.com>
2018-07-07 21:24:58 +0900

    first commit

--- app.js
+++ app.js
@@ -6,72 +6,76 @@ var express = require('express')
   , io = require('socket.io').listen(server);
 
 //设置日志级别
-io.set('log level', 1); 
+io.set('log level', 1);
 
 //WebSocket连接监听
 io.on('connection', function (socket) {
-  socket.emit('open');//通知客户端已连接
+
+  socket.emit('open',function (){
+    console.log("HELLO");
+  });//通知客户端已连接ß
+  // socket.emit('test_event','aaaa');//测试信息
 
   // 打印握手信息
-  // console.log(socket.handshake);
+  //console.log(socket.handshake);
 
   // 构造客户端对象
   var client = {
-    socket:socket,
-    name:false,
-    color:getColor()
+    socket: socket,
+    name: false,
+    color: getColor()
   }
-  
+
   // 对message事件的监听
-  socket.on('message', function(msg){
-    var obj = {time:getTime(),color:client.color};
+  socket.on('message', function (msg) {
+    var obj = { time: getTime(), color: client.color };
 
     // 判断是不是第一次连接，以第一条消息作为用户名
-    if(!client.name){
-        client.name = msg;
-        obj['text']=client.name;
-        obj['author']='System';
-        obj['type']='welcome';
-        console.log(client.name + ' login');
+    if (!client.name) {
+      client.name = msg;
+      obj['text'] = client.name;
+      obj['author'] = 'System';
+      obj['type'] = 'welcome';
+      console.log(client.name + ' login');
 
-        //返回欢迎语
-        socket.emit('system',obj);
-        //广播新用户已登陆
-        socket.broadcast.emit('system',obj);
-     }else{
+      //返回欢迎语
+      socket.emit('system', obj);
+      //广播新用户已登陆
+      socket.broadcast.emit('system', obj);
+    } else {
 
-        //如果不是第一次的连接，正常的聊天消息
-        obj['text']=msg;
-        obj['author']=client.name;      
-        obj['type']='message';
-        console.log(client.name + ' say: ' + msg);
+      //如果不是第一次的连接，正常的聊天消息
+      obj['text'] = msg;
+      obj['author'] = client.name;
+      obj['type'] = 'message';
+      console.log(client.name + ' say: ' + msg);
 
-        // 返回消息（可以省略）
-        socket.emit('message',obj);
-        // 广播向其他用户发消息
-        socket.broadcast.emit('message',obj);
-      }
-    });
+      // 返回消息（可以省略）
+      socket.emit('message', obj);
+      // 广播向其他用户发消息
+      socket.broadcast.emit('message', obj);
+    }
+  });
 
-    //监听出退事件
-    socket.on('disconnect', function () {  
-      var obj = {
-        time:getTime(),
-        color:client.color,
-        author:'System',
-        text:client.name,
-        type:'disconnect'
-      };
+  //监听出退事件
+  socket.on('disconnect', function () {
+    var obj = {
+      time: getTime(),
+      color: client.color,
+      author: 'System',
+      text: client.name,
+      type: 'disconnect'
+    };
 
-      // 广播用户已退出
-      socket.broadcast.emit('system',obj);
-      console.log(client.name + 'Disconnect');
-    });
-  
+    // 广播用户已退出
+    socket.broadcast.emit('system', obj);
+    console.log(client.name + 'Disconnect');
+  });
+
 });
 
 //express基本配置
-app.configure(function(){
+app.configure(function () {
   app.set('port', process.env.PORT || 3000);
   app.set('views', __dirname + '/views');
   app.use(express.favicon());
@@ -82,27 +86,28 @@ app.configure(function(){
   app.use(express.static(path.join(__dirname, 'public')));
 });
 
-app.configure('development', function(){
+app.configure('development', function () {
   app.use(express.errorHandler());
 });
 
 // 指定webscoket的客户端的html文件
-app.get('/', function(req, res){
-  res.sendfile('views/chat.html');
+app.get('/', function (req, res) {
+  console.log("HELLO");
+  res.sendfile('views/index.html');
 });
 
-server.listen(app.get('port'), function(){
+server.listen(app.get('port'), function () {
   console.log("Express server listening on port " + app.get('port'));
 });
 
 
-var getTime=function(){
+var getTime = function () {
   var date = new Date();
-  return date.getHours()+":"+date.getMinutes()+":"+date.getSeconds();
+  return date.getHours() + ":" + date.getMinutes() + ":" + date.getSeconds();
 }
 
-var getColor=function(){
-  var colors = ['aliceblue','antiquewhite','aqua','aquamarine','pink','red','green',
-                'orange','blue','blueviolet','brown','burlywood','cadetblue'];
+var getColor = function () {
+  var colors = ['aliceblue', 'antiquewhite', 'aqua', 'aquamarine', 'pink', 'red', 'green',
+    'orange', 'blue', 'blueviolet', 'brown', 'burlywood', 'cadetblue'];
   return colors[Math.round(Math.random() * 10000 % colors.length)];
 }
--- public/javascripts/chat.js
+++ public/javascripts/chat.js
@@ -1,4 +1,4 @@
- $(function () {
+$(function () {
     var content = $('#content');
     var status = $('#status');
     var input = $('#input');
@@ -6,31 +6,37 @@
 
     //建立websocket连接
     socket = io.connect('http://localhost:3000');
+
     //收到server的连接确认
-    socket.on('open',function(){
+    socket.on('open', function () {
         status.text('Choose a name:');
     });
 
+    //打印测试信息
+    socket.on('test_event', function (test) {
+        alert(test);
+    });
+
     //监听system事件，判断welcome或者disconnect，打印系统消息信息
-    socket.on('system',function(json){
+    socket.on('system', function (json) {
         var p = '';
-        if (json.type === 'welcome'){
-            if(myName==json.text) status.text(myName + ': ').css('color', json.color);
-            p = '<p style="background:'+json.color+'">system  @ '+ json.time+ ' : Welcome ' + json.text +'</p>';
-        }else if(json.type == 'disconnect'){
-            p = '<p style="background:'+json.color+'">system  @ '+ json.time+ ' : Bye ' + json.text +'</p>';
+        if (json.type === 'welcome') {
+            if (myName == json.text) status.text(myName + ': ').css('color', json.color);
+            p = '<p style="background:' + json.color + '">system  @ ' + json.time + ' : Welcome ' + json.text + '</p>';
+        } else if (json.type == 'disconnect') {
+            p = '<p style="background:' + json.color + '">system  @ ' + json.time + ' : Bye ' + json.text + '</p>';
         }
-        content.prepend(p); 
+        content.prepend(p);
     });
 
     //监听message事件，打印消息信息
-    socket.on('message',function(json){
-        var p = '<p><span style="color:'+json.color+';">' + json.author+'</span> @ '+ json.time+ ' : '+json.text+'</p>';
+    socket.on('message', function (json) {
+        var p = '<p><span style="color:' + json.color + ';">' + json.author + '</span> @ ' + json.time + ' : ' + json.text + '</p>';
         content.prepend(p);
     });
 
     //通过“回车”提交聊天信息
-    input.keydown(function(e) {
+    input.keydown(function (e) {
         if (e.keyCode === 13) {
             var msg = $(this).val();
             if (!msg) return;
